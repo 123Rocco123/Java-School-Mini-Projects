ebeebdcbc20c30f720aaa98dd94e134b9750b86a
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-11-25 16:57:25 +0000

    Project 8

---
+++ Mini Project 8/operators.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class operators {
+  public ArrayList expressNumbers = new ArrayList();
+
+  public static void expressNumberFunc(){
+    expressNumbers.add("A");
+    expressNumbers.add(10);
+    expressNumbers.add("B");
+    expressNumbers.add(11);
+    expressNumbers.add("C");
+    expressNumbers.add(12);
+  }
+}
