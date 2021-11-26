5582f64a3dd40e88113951e100dcd06a1a69aad3
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-11-26 12:55:34 +0000

    Algorithm rework

--- Mini Project 8/test.java
+++ Mini Project 8/test.java
@@ -3,30 +3,52 @@ import java.util.ArrayList;
 
 class test {
   public static void characterChecker(ArrayList<Character> expression) {
+    ArrayList<Character> newExpression = expression;
+    ArrayList update = new ArrayList();
+    ArrayList<Integer> indexValues = new ArrayList<Integer>();
+
+    System.out.println(newExpression);
+
     int conversion = 0;
     int multiplication = 0;
     int addition = 0;
 
-    for (int i = 0; i < expression.size(); i++) {
-      if(expression.get(i).equals('A')) {
+    for (int i = 0; i < newExpression.size(); i++) {
+      update.add(newExpression.get(i));
+
+      if (newExpression.get(i).equals('A')) {
         conversion += 10;
-      } else if (expression.get(i).equals('B')) {
+
+        update.set(i, 10);
+      } else if (newExpression.get(i).equals('B')) {
         conversion += 11;
-      } else if (expression.get(i).equals('C')) {
+
+        update.set(i, 11);
+      } else if (newExpression.get(i).equals('C')) {
         conversion += 12;
+
+        update.set(i, 12);
       }
     }
 
-    for (int i = 0; i < expression.size(); i++) {
-      if (expression.get(i).equals('*')) {
-        multiplication += (expression.get(i + 1) * expression.get(i + 2));
-      } else if (expression.get(i).equals('T')) {
-        for (int x = 0; x < 3; x++) {
-          int Tvariable = Integer.parseInt(String.valueOf(expression.get(i + 1)));
-          addition += (Tvariable + x);
+    for (int x = 0; x < newExpression.size(); x++) {
+      if (newExpression.get(x).equals('*')) {
+        int y = Character.getNumericValue(newExpression.get(x + 1));
+        int z = Character.getNumericValue(newExpression.get(x + 2));
+
+        update.set(x, (y*z));
+
+        for (int l = 0; l < 2; l++) {
+          update.remove((x + 1));
         }
+
+      } else if (newExpression.get(x).equals('T')) {
+        
       }
     }
+
+    System.out.println(update);
+
 
     System.out.println(conversion);
     System.out.println(multiplication);
@@ -34,9 +56,9 @@ class test {
 
   }
 
-  public static ArrayList<Character> mathFunction(){
+  public static ArrayList mathFunction(){
     Scanner input = new Scanner(System.in);
-    ArrayList<Character> digits = new ArrayList<Character>();
+    ArrayList<Character> digits = new ArrayList();
 
     System.out.println("Please input the expression ");
     String expression = input.nextLine();
