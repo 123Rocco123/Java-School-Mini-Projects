49a91f1464818295c4352e795adaa3294ab2ef40
123Rocco123 <petruccio.rocco@gmail.com>
2021-12-07 22:13:37 +0000

    Iterate Program and Added Conditional Statement

--- 6th Programming Project/test.java
+++ 6th Programming Project/test.java
@@ -1,9 +1,35 @@
 // While Loop Short Assessment
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class test {
+  // The function is used to output the most on time train, and the total minutes that the trains were late by.
+     // The arguments which are passed into the function are: The least late train, the total minutes that the trains were collectively late by, the least lates train starting position.
+  public static void codeExecuted(int leastLate, int totalMins, String startingPosition, ArrayList<String> leastLateTrains) {
+    String time = "";
+
+    // To make the string look nice, if the minutes that the train is late by are 1 or 0, then the variable "time" will equal minute, if not, then it will be minutes.
+    if (leastLate == 1 || leastLate == 0) {
+      time = " minute";
+    } else {
+      time = " minutes";
+    }
+
+    // The if-else statement is used to print out different messages when more than one train was the least late.
+       // Meaning when more than one train was on schedule, the if statement is executed, if only one train is the most on schedule, then the else statement is used.
+    if (leastLateTrains.size() > 0) {
+      System.out.println("\nThe trains were in total " + totalMins + " minutes late.");
+      System.out.println("The most punctual trains were to " + startingPosition + ". They were " + leastLate + time + " late.");
+    } else {
+      System.out.println("\nThe trains were in total " + totalMins + " minutes late.");
+      System.out.println("The most punctual train was to " + startingPosition + ". It was " + leastLate + time + " late.");
+    }
+  }
+
+  // The function below contains the while loop which asks the user information about the trains.
   public static void whileFunc() {
     Scanner input = new Scanner(System.in);
+    ArrayList<String> leastLateTrains = new ArrayList<String>();
 
     // The code variable is used as the condition for the while loop.
     boolean code = false;
@@ -21,18 +47,7 @@ class test {
 
       // The if condition is used to see if the user input is equal to the string "XXXX". If it is, then the loop will be broken, and the least late train will be outputted.
       if (startPoint.equals("XXXX") && leastLate != null) {
-        String time = "";
-
-        // To make the string look nice, if the minutes that the train is late by are 1 or 0, then the variable "time" will equal minute, if not, then it will be minutes.
-        if (leastLate == 1 || leastLate == 0) {
-          time = " minute";
-        } else {
-          time = " minutes";
-        }
-
-        System.out.println("");
-        System.out.println("The trains were in total " + totalMins + " minutes late.");
-        System.out.println("The most punctual train was to " + startingPosition + ". It was " + leastLate + time + " late.");
+        codeExecuted(leastLate, totalMins, startingPosition, leastLateTrains);
 
         // Changing the value of "code" to true will break the loop.
         code = true;
@@ -44,7 +59,7 @@ class test {
         System.out.println("Error, you haven't inputted any trains.\n");
 
         // The "continue" keyword is used to skip the rest of the loop once the "XXXX" condition has been met.
-           // If we were to use a function call instead, then we'd have a bug where once we use the "XXXX" code, the rest of the code below, would be executed. 
+           // If we were to use a function call instead, then we'd have a bug where once we use the "XXXX" code, the rest of the code below, would be executed.
         continue;
       }
 
@@ -58,12 +73,17 @@ class test {
       // The following if statements are used to determine what times and places have to be saved.
          // If the value of the variable is equal to null, then anything that can be assigned is assgined.
          // If instead the value isn't equal to null and the current saved value is greater than the new one, then the new one will replace the old.
+         // The last if-else statement is used when two trains have the same time.
       if (leastLate == null) {
         leastLate = lateMins;
         startingPosition = startPoint;
       } else if (leastLate != null && leastLate > lateMins) {
         leastLate = lateMins;
         startingPosition = startPoint;
+      } else if (leastLate != null & leastLate == lateMins) {
+        leastLateTrains.add(startPoint);
+
+        startingPosition += " and " + startPoint;
       }
 
       // This is used to get rid of the "\n" left by the "input.nextInt();".
